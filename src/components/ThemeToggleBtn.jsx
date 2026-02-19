144b20a1f95f414ac35a2111f5565946f8fa58b5
Fahima Azizi <fahimaazizi397@gmail.com>
2026-02-19 22:36:54 +0500

    update

--- src/components/ThemeToggleBtn.jsx
+++ src/components/ThemeToggleBtn.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import assets from '../assets/assets'
 
-function ThemeToggleBtn() {
+function ThemeToggleBtn = ({theme , setTheme}) => {
   return (
-    <div>ThemeToggleBtn</div>
+    <>
+    <button>
+        {theme == 'dark'? (
+            <img onClick={()=> setTheme('light')} src={assets.sun_icon} alt=""
+            className='size-8.5 p-a.5 border border-gray-500 rounded-full' />
+        ) : (
+            <img src={assets.mo}/>
+        )}
+    </button>
+    </>
   )
 }
 
