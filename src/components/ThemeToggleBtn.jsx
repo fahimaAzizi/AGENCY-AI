63695dc08f7d49dc39b08709b367f77176a90dde
Fahima Azizi <fahimaazizi397@gmail.com>
2026-02-20 21:45:20 +0500

    update

--- src/components/ThemeToggleBtn.jsx
+++ src/components/ThemeToggleBtn.jsx
@@ -1,32 +1,49 @@
-import React, { useEffect } from 'react'
-import assets from '../assets/assets'
+import React, { useEffect } from "react";
+import assets from "../assets/assets";
 
-const ThemeToggleBtn = ({theme , setTheme}) => {
-    useEffect(()=>{
-        const perfersDarkMode = window.matchMedia(('perfers-color-scheme:dark')).
-        matches;
-    })
+const ThemeToggleBtn = ({ theme, setTheme }) => {
 
-    useEffect(()=>{
-        if(theme == 'dark'){
-            document.documentElement.classList.add('dark')
-        }else{
-             document.documentElement.classList.remove('dark')
-        }
-        localStorage.setTheme('theme', theme)
-    },[theme])
+  // Detect system theme on first load
+  useEffect(() => {
+    const prefersDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+
+    const savedTheme = localStorage.getItem("theme");
+
+    setTheme(savedTheme || (prefersDarkMode ? "dark" : "light"));
+  }, []);
+
+  // Apply theme to HTML + save in localStorage
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
-    <>
     <button>
-        {theme == 'dark'? (
-            <img onClick={()=> setTheme('light')} src={assets.sun_icon} alt=""
-            className='size-8.5 p-a.5 border border-gray-500 rounded-full' />
-        ) : (
-            <img onClick={()=> setTheme('dark')} src={assets.moon_icon} className='size-8.5 p-a.5 border border-gray-500 rounded-full'/>
-        )}
+      {theme === "dark" ? (
+        <img
+          onClick={() => setTheme("light")}
+          src={assets.sun_icon}
+          alt=""
+          className="w-8 p-1 border border-gray-500 rounded-full cursor-pointer"
+        />
+      ) : (
+        <img
+          onClick={() => setTheme("dark")}
+          src={assets.moon_icon}
+          alt=""
+          className="w-8 p-1 border border-gray-500 rounded-full cursor-pointer"
+        />
+      )}
     </button>
-    </>
-  )
-}
+  );
+};
 
-export default ThemeToggleBtn
+export default ThemeToggleBtn;
