d6a675167427af7935717d81d477514e4260350d
Fahima Azizi <fahimaazizi397@gmail.com>
2026-02-22 22:14:51 +0500

    update

--- src/TrustedBy.jsx
+++ src/TrustedBy.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { company_logos } from './assets/assets'
 
 function TrustedBy() {
   return (
@@ -7,7 +8,10 @@ function TrustedBy() {
         <h3 className='font-semibold'> Trusted by leading companies</h3>
 
         <div className='flex items-center justify-center flex-wrap gap-10 m-4'>
-            {}
+            {company_logos.map((logo , index)=>(
+                <img key={index} src={logo} alt='' className='max-h-5 sm:max-h-6 
+                dark:drop-shadw-xl' />
+            ))}
         </div>
     </div>
   )
