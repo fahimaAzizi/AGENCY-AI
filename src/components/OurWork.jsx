a8722941ce8d2423387a2eb8349eb9c79ed7856b
Fahima Azizi <fahimaazizi397@gmail.com>
2026-02-26 22:32:10 +0500

    update

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Title from './Title'
 
-function OurWork() {
+const OurWork = () => {
   return (
-    <div>OurWork</div>
+    <div
+      id='our-work'
+      className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'
+    >
+      <Title
+        title='Our latest work'
+        desc='From strategy to execution, we craft digital solutions that move your business forward.'
+      />
+    </div>
   )
 }
 
