58366a06b41b3385e1c3b50ae9eb100b6d2ba04a
Fahima Azizi <fahimaazizi397@gmail.com>
2026-02-27 19:57:06 +0500

    update

--- src/components/TrustedBy.jsx
+++ src/components/TrustedBy.jsx
@@ -5,8 +5,7 @@ function TrustedBy() {
   return (
     <div>
         
-        <h3 className='font-semibold'> Trusted by leading companies</h3>
-
+        
         <div className='flex items-center justify-center flex-wrap gap-10 m-4'>
             {company_logos.map((logo , index)=>(
                 <img key={index} src={logo} alt='' className='max-h-5 sm:max-h-6 
