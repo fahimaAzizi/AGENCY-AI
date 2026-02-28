df83013469b17a139912545771848cfc9e84f3c7
Fahima Azizi <fahimaazizi397@gmail.com>
2026-02-28 22:10:07 +0500

    update

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import Title from './Title'
+import assets from '../assets/assets'
 
 const OurWork = () => {
 
- const workData = [
-  {
-    title: 'Mobile app marketing',
-    description:
-      'We turn bold ideas into powerful digital solutions that connect, engage...',
-    image: assets.work_mobile_app,
-  },
-  {
-    title: 'Dashboard management',
-    description:
-      'We help you execute your plan and deliver results.',
-    image: assets.work_dashboard_management,
-  },
-  {
-    title: 'fitness app promotion',
-    description:
-      'We turn bold ideas into powerful digital solutions that connect, engage...',
-    image: assets.work_fitness_app,
-  },
-]
+  const workData = [
+    {
+      title: 'Mobile app marketing',
+      description:
+        'We turn bold ideas into powerful digital solutions that connect, engage...',
+      image :assets.work_mobile_app
+    },
+    {
+      title: 'Dashboard management',
+      description:
+        'We help you execute your plan and deliver results.',
+      image: assets.work_dashboard_management,
+    },
+    {
+      title: 'fitness app promotion',
+      description:
+        'We turn bold ideas into powerful digital solutions that connect, engage...',
+      image: assets.work_fitness_app,
+    },
+  ]
 
   return (
     <div
@@ -33,17 +34,19 @@ const OurWork = () => {
         title='Our latest work'
         desc='From strategy to execution, we craft digital solutions that move your business forward.'
       />
-      <div>
-        {
-          workData.map((work ,index)=>(
-            <div key={index} className='hover:scale-102 duration-500 transition-all cursor-ponter'>
-              <img src={work.image} className='w-full rounded-xl' alt="" />
-              <h3 className='mt-3 mb-2 text-lg font-semibold'>{work.title}</h3>
-              <p className='text-sm opacity-60 w-5/6'>{work.description}</p>
-            </div>
-          ))
-        }
 
+      {/* GRID LAYOUT (recommended) */}
+      <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-10'>
+        {workData.map((work, index) => (
+          <div
+            key={index}
+            className='hover:scale-105 duration-500 transition-all cursor-pointer'
+          >
+            <img src={work.image} className='w-full rounded-xl' alt="" />
+            <h3 className='mt-3 mb-2 text-lg font-semibold'>{work.title}</h3>
+            <p className='text-sm opacity-60 w-5/6'>{work.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   )
