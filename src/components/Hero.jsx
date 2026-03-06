6dab3cbf59150d8354b30c289f42e9b8c1f75f86
Fahima Azizi <fahimaazizi397@gmail.com>
2026-03-06 18:44:35 +0500

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,26 +16,41 @@ const Hero = () => {
         <p className='text-xs font-medium'>Trusted by 10k+ people</p>
       </motion.div>
 
-      <h1 className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl'>
+      <motion.h1
+       initial ={{opacity : 0, y: 40}}
+      whileInView={{opacity :1 ,y:0}}
+      transition={{ duration: 0.6, delay:0.8}}
+      viewport={{once: true}}
+       className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl'>
         Turning imagination into{" "}
         <span className='bg-gradient-to-r from-[#5044E5] to-[#4d8cea] bg-clip-text text-transparent'>
           digital
         </span>{" "}
         impact.
-      </h1>
+      </motion.h1>
 
-      <p className='text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-[80%] sm:max-w-lg pb-3'>
+      <motion.p 
+      initial ={{opacity : 0, y: 30}}
+      whileInView={{opacity :1 ,y:0}}
+      transition={{ duration: 0.5, delay:1}}
+      viewport={{once: true}}
+      className='text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-[80%] sm:max-w-lg pb-3'>
         Creating meaningful connections and turning big ideas into interactive digital experiences.
-      </p>
+      </motion.p>
 
-      <div className='relative'>
+      <motion.div
+       initial ={{opacity : 0, y: 0.9}}
+      whileInView={{opacity :1 ,y:0}}
+      transition={{ duration: 0.5, delay: 2}}
+      viewport={{once: true}}
+       className='relative'>
         <img src={assets.hero_img} alt="" className='w-full max-w-6xl' />
         <img
           src={assets.bgImage1}
           alt=""
           className='absolute -top-40 -right-40 sm:-top-100 sm:-right-70 -z-10 dark:hidden'
         />
-      </div>
+      </motion.div>
 
     </div>
   )
