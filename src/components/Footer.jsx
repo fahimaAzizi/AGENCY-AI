1e6c21bb34eb779f4997daa7bf0b0a5a2f5fdac5
Fahima Azizi <fahimaazizi397@gmail.com>
2026-03-10 23:36:33 +0500

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import assets from '../assets/assets'
+import { motion } from 'motion/react'
+
 
 const Footer = () => {
   return (
-    <div className='bg-slate-50 dark:bg-gray-900 pt-10 sm:pt-10 mt-20 sm:mt-40 px-4 sm:px-10 lg:px-24 xl:px-40'>
+    <motion.div
+    initial ={{opacity : 0, y: 50}}
+      whileInView={{opacity :1 ,y:0}}
+      transition={{ duration: 0.8}}
+      viewport={{once: true}} 
+      className='bg-slate-50 dark:bg-gray-900 pt-10 sm:pt-10 mt-20 sm:mt-40 px-4 sm:px-10 lg:px-24 xl:px-40'>
 
       {/* footer top */}
 
       <div className='flex justify-between lg:items-center max-lg:flex-col gap-10'>
 
-        <div className='space-y-5 text-sm text-gray-700 dark:text-gray-400'>
+        <motion.div
+        initial ={{opacity : 0, y: -30}}
+      whileInView={{opacity :1 ,y:0}}
+      transition={{ duration: 0.6 , delay : index * 0.2}}
+      viewport={{once: true}}
+         className='space-y-5 text-sm text-gray-700 dark:text-gray-400'>
 
           {/* Light Logo */}
           <img 
@@ -47,7 +59,7 @@ const Footer = () => {
             </li>
           </ul>
 
-        </div>
+        </motion.div>
         <div className='text-gray-600 dark:text-gray-400'>
   <h3 className='font-semibold'>
     Subscribe to our newsletter
@@ -72,7 +84,7 @@ const Footer = () => {
 
       </div>
 
-    </div>
+    </motion.div>
   )
 }
 
