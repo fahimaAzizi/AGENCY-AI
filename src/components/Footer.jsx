3eb61553e817e0b30040f866a49f16952158fce4
Fahima Azizi <fahimaazizi397@gmail.com>
2026-03-10 23:45:12 +0500

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -89,7 +89,21 @@ const Footer = () => {
 
       </div>
       <hr  className='border-gray-300 dark:border-gray-600 my-6'/>
+      <motion.div
+       initial ={{opacity : 0}}
+      whileInView={{opacity :1}}
+      transition={{ duration: 0.5, delay: 0.4}}
+      viewport={{once: true}}
+       className='pb-6 text-sm text-gray-500 flex justify-center sm:justify-between gap-4 flex-wrap'>
+  <p> 2026  All Right Reserved.</p>
 
+  <div className='flex items-center justify-between gap-4'>
+    <img src={assets.facebook_icon} alt="" />
+    <img src={assets.twitter_icon} alt="" />
+    <img src={assets.instagram_icon} alt="" />
+    <img src={assets.linkedin_icon} alt="" />
+  </div>
+</motion.div>
     </motion.div>
   )
 }
